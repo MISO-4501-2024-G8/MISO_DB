196f2ec740f54de21172f4dc04f61a12f2b0c1cd
alexyirsa <v.yirsa@uniandes.edu.co>
2024-04-03 22:57:29 -0500

    Create plan.sql
    Creación de SQL para la tabla plan

---
+++ TABLES/plan.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `plan` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `typePlan` varchar(15) NOT NULL,
+  `startDate` datetime NOT NULL,
+  `endDate` datetime NOT NULL,
+  `value` float NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `typePlan_UNIQUE` (`typePlan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
