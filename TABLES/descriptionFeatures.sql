175f89d43669cf4ac772e48baf02741f58f31f3a
alexyirsa <v.yirsa@uniandes.edu.co>
2024-04-03 22:59:23 -0500

    Update descriptionFeatures.sql
    Modificación sql

--- TABLES/descriptionFeatures.sql
+++ TABLES/descriptionFeatures.sql
@@ -1,4 +1,8 @@
-CREATE TABLE descriptionFeatures
-	(tipoPlan VARCHAR(45) NOT NULL,
-	description VARCHAR(15) NOT NULL,
-	foreign key (tipoPlan) references plan(typePlan))
+CREATE TABLE `descriptionFeatures` (
+  `id` int NOT NULL,
+  `tipoPlan` varchar(45) NOT NULL,
+  `description` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tipoPlan` (`tipoPlan`),
+  CONSTRAINT `descriptionFeatures_ibfk_1` FOREIGN KEY (`tipoPlan`) REFERENCES `plan` (`typePlan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
