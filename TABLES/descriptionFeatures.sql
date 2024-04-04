63f17356c4f05b32321ea894da564480d390323a
alexyirsa <v.yirsa@uniandes.edu.co>
2024-04-03 22:49:31 -0500

    Create descriptionFeatures.sql
    Creación de archivo con consulta SQL de creación para la tabla descriptionFeatures

---
+++ TABLES/descriptionFeatures.sql
@@ -0,0 +1,3 @@
+CREATE TABLE descriptionFeatures(tipoPlan VARCHAR(45) NOT NULL,
+								                description VARCHAR(15) NOT NULL,
+                                foreign key (tipoPlan) references plan(typePlan))
