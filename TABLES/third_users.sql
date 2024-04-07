5be6a32a27069554f424674d1b67a599231ea06a
Andres Cardenas <a.cardenasj@uniandes.edu.co>
2024-04-07 18:18:12 -0500

    Update Third_user

---
+++ TABLES/third_users.sql
@@ -0,0 +1,8 @@
+
+
+CREATE TABLE third_users (
+    id VARCHAR(255) PRIMARY KEY,
+    company_creation_date DATETIME NOT NULL,
+    company_address VARCHAR(255), 
+    contact_name VARCHAR(255) NOT NULL,
+);
